6399cb1a8500ffccc67db738d2d6dad42ca68aa8
paulomcs <52682759+paulomcs@users.noreply.github.com>
2022-05-13 23:55:49 -0300

    Delete src directory

--- src/data/film.js
+++
@@ -1,9 +0,0 @@
-export default function dadosFilme(filmes,type){
-    
-    for(let i = 0; i < filmes.length; i++){
-        if (filmes[i].id === type){
-            return filmes[i]
-        }
-    }
-
-}
